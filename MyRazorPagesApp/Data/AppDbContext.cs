73fd5a4c878ad9cc6ad3cc5c46089b442cde513a
Hesoyi <c2011080@student.cankaya.edu.tr>
2024-05-24 16:27:23 +0300

    Lab9 yapıldı database bağlandı tablo gözlemlendi SQL serverde

---
+++ MyRazorPagesApp/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MyRazorPagesApp.Model;
+namespace MyRazorPagesApp.Data{
+public class AppDbContext : DbContext
+{
+public AppDbContext(DbContextOptions<AppDbContext> options) :
+base(options)
+{
+}
+public DbSet<Room> Rooms { get; set; }
+}
+}
